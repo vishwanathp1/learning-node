15deba0a371d8c83e348775a46a6996cf755dbff
Vishwanath <vishwanath95@gmail.com>
2019-09-22 17:18:03 +0530

    modeules comment

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var a = 2;
 //in an immedeately invoked function expression like below,
 //the scope of the variables create are limited to the 
 //function itself! So outside the function, a remains 3.
+// this is how modules are made
 (function(){
     var a =3;
     console.log(a);
