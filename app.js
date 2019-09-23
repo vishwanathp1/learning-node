a50c29b6f68f52543e05ab7b330db91287ab8128
Vishwanath <vishwanath95@gmail.com>
2019-09-23 12:33:05 +0530

    editing html upon getting an http requrest

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(
     function(req, res) {
-        res.writeHead( 200, {'Content-Type': 'text/plain'});
-        res.end('Hello World\n');
+        res.writeHead( 200, {'Content-Type': 'text/html'});
+        var html = fs.readFileSync(__dirname + '/index.html', 'utf8')
+        var message = 'hello now';
+        html = html.replace('{Message}', message);
+        res.end(html);
     }
 ).listen(1337, '127.0.0.1');
