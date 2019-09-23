36276326c9fedc47af0d1b60d0aaab7923bc2dc1
Vishwanath <vishwanath95@gmail.com>
2019-09-23 13:07:38 +0530

    adding developer dependencies

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-var http = require('http');
-var fs = require('fs');
-
-http.createServer(
-    function(req, res) {
-        res.writeHead( 200, {'Content-Type': 'text/html'});
-        var obj = {
-            f: 'J',
-            l: 'D'
-        };
-        res.end(JSON.stringify(obj));
-    }
-).listen(1337, '127.0.0.1');
+var moment = require('moment');
